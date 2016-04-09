1a46632cd46147207d7b7203e31933438f942a57
Larry Chong <l4chong@gmail.com>
2016-04-09 13:21:38 -0400

    add listener

--- app/src/main/java/flybits/com/flybits/ui/ApplicationActivity.java
+++ app/src/main/java/flybits/com/flybits/ui/ApplicationActivity.java
@@ -1,11 +1,15 @@
-package flybits.com.flybits;
+package flybits.com.flybits.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import flybits.com.flybits.R;
 import flybits.com.flybits.adapter.ContactAdapter;
 import flybits.com.flybits.app.AppController;
 import flybits.com.flybits.model.FBFriend;
@@ -33,5 +37,17 @@ public class ApplicationActivity extends AppCompatActivity {
 
         ContactAdapter adapter = new ContactAdapter(this, Friends);
         listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ApplicationActivity.this, "touch works!!", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(this, DisplayMessageActivity.class);
+//                Bundle b = new Bundle();
+//                b.putString("photourl", imageurl);
+//                intent.putExtras(b);
+//                startActivity(intent);
+            }
+        });
     }
 }
